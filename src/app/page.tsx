4f42abb529667efd4a0ba478b183a4c92962fb1e
Lisette Svan <svanlisette@gmail.com>
2024-12-04 11:45:10 +0100

    never ending changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,29 @@
 'use client'
-import Image from "next/image";
-import { useUserContext } from '@/utils/contexts'
-import { RecipeType, UserContextType } from "@/utils/types";
 import { useEffect, useState } from "react";
+import { useUserContext } from '@/utils/contexts';
+import { RecipeType, UserContextType } from "@/utils/types";
 import Link from "next/link";
 
 export default function Home() {
-  const { user, updateUser } = useUserContext() as UserContextType
-  const [recipes, setRecipes] = useState<RecipeType[] | null>(null)
+  const { user, updateUser } = useUserContext() as UserContextType;
+  const [recipes, setRecipes] = useState<RecipeType[]>([]); 
 
-  useEffect( () => {
+  useEffect(() => {
     const fetchRecipes = async () => {
       try {
         if (user) {
-        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${user.category}`)
-        const data = await response.json();
-        console.log(data)
-        console.log(data.meals)
+          const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${user.category}`);
+          const data = await response.json();
+          const topFiveRecipes = data.meals.slice(0, 5); 
 
-        const topFiveRecipes = data.meals.slice(0, 5);
-      
-        setRecipes(topFiveRecipes)
+          setRecipes(topFiveRecipes);
         }
-      } catch(error) {
-        console.log(error)
+      } catch (error) {
+        console.log(error);
       }
-    } 
-  fetchRecipes()
-  }, [user])
+    };
+    fetchRecipes();
+  }, [user]);
 
   const toggleFavourite = (meal: RecipeType) => {
     if (user) {
@@ -39,18 +35,18 @@ export default function Home() {
         updateUser({ ...user, savedRecipes: [...user.savedRecipes, meal] });
       }
     }
-  };  
-  
+  };
+
   return (
     <>
-{ user && (
+      {user && (
         <div className="text-black font-light text-2xl m-10">
           The food category you have chosen is: {user.category}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-4">
-            {recipes && recipes.map((meal: RecipeType) => (
+            {recipes.map((meal: RecipeType) => (
               <div className="group relative" key={meal.idMeal}>
                 <Link href={`/food/${meal.idMeal}`}>
-                  <img className="w-full h-auto rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-xl" src={meal.strMealThumb} height="auto" width="200px"></img>
+                  <img className="w-full h-auto rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-xl" src={meal.strMealThumb} alt={meal.strMeal} height="auto" width="200px" />
                 </Link>
                 <p className="text-gray-600 text-lg font-light mt-4">{meal.strMeal}</p>
                 <button onClick={() => toggleFavourite(meal)} 
